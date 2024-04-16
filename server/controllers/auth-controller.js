ba8d5aeed5ac1d2972993cde3204e1ce7f72a2b7
Dev Harsh <devharsh466@gmail.com>
2024-04-16 17:33:47 +0530

    connecting to db

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -20,7 +20,10 @@ const home = async (req, res) => {
   // *-------------------
   const register = async (req, res) => {
     try {
-      res.status(200).send({ message: "registration page" });
+      const data = req.body;
+      console.log(req.body);
+      // res.status(201).json({ message: "User registered successfully" });
+      res.status(200).json({ msg: data });
     } catch (error) {
       res.status(500).json({ message: "Internal server error" });
     }
--- server/server.js
+++ server/server.js
@@ -1,11 +1,22 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const router = require("./routes/auth-route");
+const connectDb = require("./utils/db");
+
+/*This line of code adds Express middleware that parses incoming request bodies with JSON payloads.
+It's important to place this before any routes that need to handle JSON data in the request body. 
+This middleware is responsible for parsing JSON data from requests, 
+and it should be applied at the beginning of your middleware stack to ensure it's available for all subsequent route handlers.*/
+
+app.use(express.json());
 
 // Mount the Router: To use the router in your main Express app, you can "mount" it at a specific URL prefix
 app.use("/api/auth", router);
 
 const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`server is running at port: ${PORT}`);
-});
+connectDb().then(() => {
+    app.listen(PORT, () => {
+      console.log(`server is running at port: ${PORT}`);
+    });
+  });
