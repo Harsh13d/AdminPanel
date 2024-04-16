360015305ecc285b779d1dc34c458fc7a84e1d43
Dev Harsh <devharsh466@gmail.com>
2024-04-16 16:54:44 +0530

    adding controllers

--- server/server.js
+++ server/server.js
@@ -1,13 +1,9 @@
 const express = require("express");
 const app = express();
+const router = require("./routes/auth-route");
 
-app.get("/", (req, res) => {
-  res.status(200).send("Welcome to thapa technical Mern Series Updated");
-});
-
-app.get("/register", (req, res) => {
-  res.status(200).json({ msg: "registration successful" });
-});
+// Mount the Router: To use the router in your main Express app, you can "mount" it at a specific URL prefix
+app.use("/api/auth", router);
 
 const PORT = 3000;
 app.listen(PORT, () => {
