30bf30e956f6aad502f92494fad49112568d0d53
Dev Harsh <devharsh466@gmail.com>
2024-04-18 09:49:29 +0530

    Contact route

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const express = require("express");
 const app = express();
 const router = require("./routes/auth-route");
 const connectDb = require("./utils/db");
+const errorMiddleware = require("./middlewares/error-middleware");
+const contactRoute = require("./routes/contact-route");
 
 /*This line of code adds Express middleware that parses incoming request bodies with JSON payloads.
 It's important to place this before any routes that need to handle JSON data in the request body. 
@@ -13,6 +15,9 @@ app.use(express.json());
 
 // Mount the Router: To use the router in your main Express app, you can "mount" it at a specific URL prefix
 app.use("/api/auth", router);
+app.use("/api/form", contactRoute);
+
+app.use(errorMiddleware);
 
 const PORT = 3000;
 connectDb().then(() => {
