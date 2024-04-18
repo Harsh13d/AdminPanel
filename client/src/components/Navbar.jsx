6d3878cfe012d873d4f1e22ef8a4eb91b6da8c56
Dev Harsh <devharsh466@gmail.com>
2024-04-19 00:17:38 +0530

    Logout functionality updated

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,10 @@
 import { NavLink } from "react-router-dom";
 import "./Navbar.css";
+import { useAuth } from "../store/auth";
 
 export const Navbar = () => {
+  const { isLoggedIn } = useAuth();
+  console.log("login or not ", isLoggedIn);
   return (
     <>
       <header>
@@ -24,12 +27,21 @@ export const Navbar = () => {
               <li>
                 <NavLink to="/contact"> Contact </NavLink>
               </li>
-              <li>
-                <NavLink to="/register"> Register </NavLink>
-              </li>
-              <li>
-                <NavLink to="/login"> Login </NavLink>
-              </li>
+
+              {isLoggedIn ? (
+                <li>
+                  <NavLink to="/logout">Logout</NavLink>
+                </li>
+              ) : (
+                <>
+                  <li>
+                    <NavLink to="/register"> Register </NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="/login"> Login </NavLink>
+                  </li>
+                </>
+              )}
             </ul>
           </nav>
         </div>
