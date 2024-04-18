84ba3a01bf15b586e02fcb811cc0a862c97e6f61
Dev Harsh <devharsh466@gmail.com>
2024-04-19 00:01:41 +0530

    Creating context Api for storing token in local storage , login page formed

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useAuth } from "../store/auth";
 
 export const Login = () => {
   const [user, setUser] = useState({
     username: "",
     password: "",
   });
+
+  const { saveTokenInLocalStr } = useAuth();
 
   const navigate = useNavigate();
 
@@ -20,6 +23,29 @@ export const Login = () => {
     });
   };
 
+  // let handle the form submission
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("http://localhost:3000/api/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(user),
+      });
+
+      if (response.ok) {
+        const responseData = await response.json();
+        console.log("after login: ", responseData);
+        // toast.success("Registration Successful");
+        saveTokenInLocalStr(responseData.token);
+        navigate("/");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <>
@@ -63,7 +89,7 @@ export const Login = () => {
                   </div>
                   <br />
                   <button type="submit" className="btn btn-submit">
-                    Register Now
+                    Login Now
                   </button>
                 </form>
               </div>
