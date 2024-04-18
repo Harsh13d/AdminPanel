5ac444d591e5d140472cc36c165e1138632bf5b1
Dev Harsh <devharsh466@gmail.com>
2024-04-18 16:33:09 +0530

    dealing with cors policy

--- server/server.js
+++ server/server.js
@@ -1,15 +1,25 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const router = require("./routes/auth-route");
 const connectDb = require("./utils/db");
 const errorMiddleware = require("./middlewares/error-middleware");
 const contactRoute = require("./routes/contact-route");
+const serviceRoute = require("./routes/service-route");
 
 /*This line of code adds Express middleware that parses incoming request bodies with JSON payloads.
 It's important to place this before any routes that need to handle JSON data in the request body. 
 This middleware is responsible for parsing JSON data from requests, 
 and it should be applied at the beginning of your middleware stack to ensure it's available for all subsequent route handlers.*/
+
+const corsOptions = {
+    origin: "http://localhost:5175",
+    methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
+    credentials: true,
+  };
+  
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
